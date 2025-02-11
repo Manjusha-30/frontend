e32bc8a9316d6c27b353660d757d08866505f739
Manjusha-30 <manjushakapade@gmail.com>
2025-02-12 00:49:04 +0530

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import ShopContextProvider from './contex/shopContext.jsx'
+import ShopContextProvider from './contex/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
